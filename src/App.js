be23de6b66e360ef5b85877eb4875b192e73abd7
CHINMAYA PRASAD BEHERA <cpbehera03@gmail.com>
2026-07-12 08:38:12 +0530

    state added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  let [todos, setTodos] = useState([]);
+
+  let saveToDoList = (e) => {
+    e.preventDefault();
+
+    let todoName = e.target.todoName.value;
+    if (!todos.includes(todoName)) {
+      setTodos([...todos, todoName]);
+      alert(`Task saved: ${todoName}`);
+    } else {
+      alert(`Task already exists: ${todoName}`);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>ToDo List</h1>
+        <form onSubmit={saveToDoList}>
+          <input type="text" name='todoName' placeholder="Add a new task" />
+          <button>Save</button>
+        </form>
       </header>
     </div>
   );
